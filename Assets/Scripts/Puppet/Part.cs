98b631c9a969f60e2397cf2de48dfe885db78997
grampush <grampush@gmail.com>
2014-10-23 22:53:06 +0900

    add Part.cs (remove Hinge.cs)

---
+++ Assets/Scripts/Puppet/Part.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Part {
+
+	public Transform transform { get; private set; }
+	public CharacterJoint joint { get; private set; }
+
+	public Part(Transform tramsform) {
+		this.transform = tramsform;
+		this.joint = tramsform.GetComponent<CharacterJoint> ();
+	}
+}
--- Assets/Scripts/Puppet/Poser.cs
+++ Assets/Scripts/Puppet/Poser.cs
@@ -3,44 +3,30 @@ using System.Collections;
 
 public class Poser : MonoBehaviour {
 
-	public Transform Root { get; private set; }
-
-	private Transform chest;
-	private Transform head;
-
-	public CharacterJoint Chest { get { return chest.GetComponent<CharacterJoint>(); } }
-	public CharacterJoint Head { get { return head.GetComponent<CharacterJoint>(); } }
-
+	public Part Root { get; private set; }
+	public Part Chest { get; private set; }
+	public Part Head { get; private set; }
 
 	// Use this for initialization
 	void Start () {
+		ReadParts ();
 
-		ReadRootPart ();
-		ReadJoints ();
-
-		Chest.rigidbody.AddForce (new Vector3 (0, 0, -30));
-		Head.rigidbody.AddForce (new Vector3 (0, 0, 10));
-	
+		Chest.transform.localEulerAngles = new Vector3 (-40, 0, 0);
+		Head.transform.localEulerAngles = new Vector3 (40, 0, 0);
 	}
 
-	void ReadRootPart () {
-		Root = transform.Find ("puppet");
-		if (!Root) {
-			Debug.LogError("root part not found");
+	void ReadParts () {
+		Root = ReadPart ("puppet");
+		Chest = ReadPart ("puppet/Chest");
+		Head = ReadPart ("puppet/Chest/Neck/Head");
+	}
+
+	Part ReadPart (string partName) {
+		Part part = new Part(transform.Find (partName));
+		if (part.transform == null) {
+			Debug.LogError("part(" + partName + ") not found");
 		}
-	}
-
-	void ReadJoints () {
-		chest = ReadJoint ("puppet/Chest");
-		head = ReadJoint ("puppet/Chest/Neck/Head");
-	}
-
-	Transform ReadJoint (string name) {
-		Transform joint = transform.Find (name);
-		if (!joint) {
-			Debug.LogError("joint (" + name + ") not found");
-		}
-		return joint;
+		return part;
 	}
 
 }
