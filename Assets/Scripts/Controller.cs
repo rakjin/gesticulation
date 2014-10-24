753a907e1f3ff500772347627b48a7b890461e73
rakjin <grampush@gmail.com>
2014-10-25 06:13:46 +0900

    add Controller.cs

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject defaultPuppet = GameObject.Find ("/Puppet");
+		Debug.Log (defaultPuppet);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
