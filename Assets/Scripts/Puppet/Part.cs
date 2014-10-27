0d9668c39b464ae95bbcb52a9cc52bed84dbe2c4
grampush <grampush@gmail.com>
2014-10-27 19:28:23 +0900

    store joint and rigidbody of each part

--- Assets/Scripts/Puppet/Part.cs
+++ Assets/Scripts/Puppet/Part.cs
@@ -6,11 +6,62 @@ public class Part : MonoBehaviour {
 	static readonly Color normalColor = new Color (0.5f, 0.5f, 0.5f);
 	static readonly Color highlightedColor = new Color (0.125f, 1, 0);
 
-	public CharacterJoint joint { get; private set; }
+	private CharacterJointMock StoredJoint { get; set; }
+	private RigidbodyMock StoredRigidbody { get; set; }
 
 	void Awake () {
-		joint = GetComponent<CharacterJoint> ();
+		StoreJoint ();
+		StoreRigidbody ();
 	}
+
+	#region Store
+
+	void StoreJoint() {
+		CharacterJoint original = GetComponent<CharacterJoint> ();
+		if (original) {
+			StoredJoint = new CharacterJointMock();
+			StoredJoint.anchor = original.anchor;
+			StoredJoint.autoConfigureConnectedAnchor = original.autoConfigureConnectedAnchor;
+			StoredJoint.axis = original.axis;
+			StoredJoint.breakForce = original.breakForce;
+			StoredJoint.breakTorque = original.breakTorque;
+			StoredJoint.connectedAnchor = original.connectedAnchor;
+			StoredJoint.connectedBody = original.connectedBody;
+			StoredJoint.highTwistLimit = original.highTwistLimit;
+			StoredJoint.lowTwistLimit = original.lowTwistLimit;
+			StoredJoint.swing1Limit = original.swing1Limit;
+			StoredJoint.swing2Limit = original.swing2Limit;
+			StoredJoint.swingAxis = original.swingAxis;
+			DestroyImmediate(original);
+		}
+	}
+
+	void StoreRigidbody() {
+		Rigidbody original = GetComponent<Rigidbody> ();
+		if(original) {
+			StoredRigidbody = new RigidbodyMock();
+			StoredRigidbody.angularDrag = original.angularDrag;
+			StoredRigidbody.angularVelocity = original.angularVelocity;
+			StoredRigidbody.centerOfMass = original.centerOfMass;
+			StoredRigidbody.constraints = original.constraints;
+			StoredRigidbody.drag = original.drag;
+			StoredRigidbody.isKinematic = original.isKinematic;
+			StoredRigidbody.mass = original.mass;
+			StoredRigidbody.maxAngularVelocity = original.maxAngularVelocity;
+			StoredRigidbody.position = original.position;
+			StoredRigidbody.rotation = original.rotation;
+			StoredRigidbody.sleepAngularVelocity = original.sleepAngularVelocity;
+			StoredRigidbody.sleepVelocity = original.sleepVelocity;
+			StoredRigidbody.useGravity = original.useGravity;
+			StoredRigidbody.velocity = original.velocity;
+			DestroyImmediate(original);
+		}
+	}
+
+	#endregion
+
+
+	#region Highlight
 
 	private bool highlighted = false;
 	public bool Highlighted {
@@ -38,4 +89,45 @@ public class Part : MonoBehaviour {
 	void OnMouseExit () {
 		Highlighted = false;
 	}
+
+	#endregion
 }
+
+
+
+#region internal class
+
+internal class CharacterJointMock {
+	public Vector3 anchor;
+	public bool autoConfigureConnectedAnchor;
+	public Vector3 axis;
+	public float breakForce;
+	public float breakTorque;
+	public Vector3 connectedAnchor;
+	public Rigidbody connectedBody;
+	public SoftJointLimit highTwistLimit;
+	public SoftJointLimit lowTwistLimit;
+	public SoftJointLimit swing1Limit;
+	public SoftJointLimit swing2Limit;
+	public Vector3 swingAxis;
+}
+
+internal class RigidbodyMock {
+	public float angularDrag;
+	public Vector3 angularVelocity;
+	public Vector3 centerOfMass;
+	public RigidbodyConstraints constraints;
+	public float drag;
+	public bool isKinematic;
+	public float mass;
+	public float maxAngularVelocity;
+	public Vector3 position;
+	public Quaternion rotation;
+	public float sleepAngularVelocity;
+	public float sleepVelocity;
+	public bool useGravity;
+	public Vector3 velocity;
+}
+
+#endregion
+
