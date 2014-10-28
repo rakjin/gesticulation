016df30c2452049bf4f589f5b3dad305d75e3e72
grampush <grampush@gmail.com>
2014-10-29 04:32:16 +0900

    draw finger gizmos on Picker.cs, not GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,10 +11,6 @@ public class GameController : MonoBehaviour {
 
 	Poser defaultPoser;
 
-	Controller controller = new Controller();
-	Vector3 controllerPosition = new Vector3(0, -0.5f, 0);
-	float controllerScale = 10;
-
 	// Use this for initialization
 	void Start () {
 
@@ -24,40 +20,10 @@ public class GameController : MonoBehaviour {
 
 		GameObject defaultPuppet = GameObject.Find ("/Puppet");
 		defaultPoser = defaultPuppet.GetComponent<Poser> ();
-
-		//defaultPoser.Pose (Pose.RandomPose01 (), 3f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	}
-
-	void OnDrawGizmos() {
-		if (controller == null) {
-			return;
-		}
-
-		Frame frame = controller.Frame ();
-		if (frame.Hands.Count > 0) {
-			Vector3 palmPosition = frame.Hands[0].PalmPosition.ToUnityScaled();
-			Vector3 thumbPosition = frame.Hands[0].Fingers[0].TipPosition.ToUnityScaled();
-			Vector3 indexPosition = frame.Hands[0].Fingers[1].TipPosition.ToUnityScaled();
-			palmPosition *= controllerScale;
-			thumbPosition *= controllerScale;
-			indexPosition *= controllerScale;
-			palmPosition += controllerPosition;
-			thumbPosition += controllerPosition;
-			indexPosition += controllerPosition;
-
-			if (Vector3.Distance(indexPosition, thumbPosition) < 0.25f) {
-				Gizmos.color = Color.red;
-				Sphere.position = (thumbPosition + indexPosition) * 0.5f;
-			}
-
-			Gizmos.DrawWireSphere(palmPosition, 0.25f);
-			Gizmos.DrawWireSphere(thumbPosition, 0.125f);
-			Gizmos.DrawWireSphere(indexPosition, 0.125f);
-		}
 	}
 
 	void OnGUI () {
--- Assets/Scripts/Picker.cs
+++ Assets/Scripts/Picker.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using Leap;
+
 public class Picker : MonoBehaviour {
+
+	FingerModel thumb;
+	FingerModel index;
 
 	// Use this for initialization
 	void Start () {
+
+		HandModel handModel = GetComponent<HandModel> ();
+		thumb = handModel.fingers [0];
+		index = handModel.fingers [1];
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
 	
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere (thumb.GetTipPosition (), 0.125f);
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere (index.GetTipPosition (), 0.125f);
 	}
 }
