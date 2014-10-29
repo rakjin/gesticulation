babcbc885a67e9b2647bd251bc86c4ec654cdb97
grampush <grampush@gmail.com>
2014-10-30 05:31:44 +0900

    add Button3D

---
+++ Assets/Scripts/UI/Button3D.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button3D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		TextMesh textMesh = gameObject.AddComponent<TextMesh> ();
+
+		textMesh.text = "안뇽?ㅋ";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
