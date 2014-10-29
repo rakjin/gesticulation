1db1f7049dcaf66ac35840b7f28b6dba631a8ef6
grampush <grampush@gmail.com>
2014-10-30 06:54:22 +0900

    sway camera slowly

---
+++ Assets/Scripts/Sway.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sway : MonoBehaviour {
+
+	Vector3 initialPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+		initialPosition = transform.localPosition;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float x = Mathf.Sin (Time.time * 0.25f) * 0.25f;
+		float y = Mathf.Cos (Time.time * 0.125f) * 0.125f;
+
+		Vector3 swayPosition = new Vector3 (
+			initialPosition.x + x,
+			initialPosition.y + y,
+			initialPosition.z);
+
+		transform.localPosition = swayPosition;
+	
+	}
+}
