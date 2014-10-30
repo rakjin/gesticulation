d71d08e174c11e833eeb9f95dcd2b838abc1d4b8
grampush <grampush@gmail.com>
2014-10-31 07:32:47 +0900

    add PresetDataSource

---
+++ Assets/Scripts/Shelf/PresetDataSource.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PresetDataSource {
+
+	private List<Preset> presets = new List<Preset>();
+	private Preset empty = new Preset(Preset.PresetType.Empty);
+
+	public PresetDataSource() {
+	}
+
+	public Preset Get(int index) {
+		if (index < 0) {
+			return empty;
+		}
+
+		if (index >= presets.Count) {
+			return empty;
+		}
+
+		return presets[index];
+	}
+
+}
