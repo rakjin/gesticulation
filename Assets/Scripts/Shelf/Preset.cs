6299a52ea6b9bdf268e9ebcf6bfc5efd0e7061c4
grampush <grampush@gmail.com>
2014-10-31 07:11:47 +0900

    add Preset.cs

---
+++ Assets/Scripts/Shelf/Preset.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Preset {
+
+	public enum PresetType {
+		Empty,
+		Static,
+		Animated,
+		NewPresetPlaceHolder,
+	}
+
+	public Pose Pose { get; private set; }
+	public string Title { get; private set; }
+	public string Author { get; private set; }
+	public PresetType Type { get; private set; }
+
+	public Preset(PresetType type = PresetType.Empty, Pose pose = null, string title = "", string author = "") {
+		Type = type;
+		Pose = pose;
+		Title = title;
+		Author = author;
+	}
+
+}
