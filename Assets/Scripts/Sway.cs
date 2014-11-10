a652d5ac887b5b8e8895b12fe08ae86064327d93
grampush <grampush@gmail.com>
2014-11-10 17:08:36 +0900

    camera sway speed

--- Assets/Scripts/Sway.cs
+++ Assets/Scripts/Sway.cs
@@ -3,21 +3,25 @@ using System.Collections;
 
 public class Sway : MonoBehaviour {
 
+	const float SPEED = 4;
+
 	Vector3 initialPosition;
+	Transform lookAtTransform;
 	Vector3 lookAt;
 
 	// Use this for initialization
 	void Start () {
 	
 		initialPosition = transform.localPosition;
-		lookAt = GameObject.Find ("/LookAt").transform.position;
+		lookAtTransform = GameObject.Find ("/LookAt").transform;
+		lookAt = lookAtTransform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		float x = Mathf.Sin (Time.time * 0.125f) * 0.25f;
-		float y = Mathf.Cos (Time.time * 0.0625f) * 0.125f;
+		float x = Mathf.Sin (Time.time * 0.125f * SPEED) * 0.25f;
+		float y = Mathf.Cos (Time.time * 0.0625f * SPEED) * 0.125f;
 
 		Vector3 swayPosition = new Vector3 (
 			initialPosition.x + x,
@@ -26,6 +30,7 @@ public class Sway : MonoBehaviour {
 
 		transform.localPosition = swayPosition;
 
+		lookAt = lookAtTransform.position;
 		transform.LookAt (lookAt);
 	
 	}
