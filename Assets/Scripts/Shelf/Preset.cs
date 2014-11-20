1b68c6ca723db5c2d02a590cb070bd5903e239bc
grampush <grampush@gmail.com>
2014-11-20 21:10:37 +0900

    change Pose field of Preset to Motion (list of poses) and add another constructor

--- Assets/Scripts/Shelf/Preset.cs
+++ Assets/Scripts/Shelf/Preset.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 public class Preset {
 
@@ -10,14 +12,40 @@ public class Preset {
 		NewPresetPlaceHolder,
 	}
 
-	public Pose Pose { get; private set; }
+	private List<Pose> motion;
+	public ReadOnlyCollection<Pose> Motion {
+		get {
+			if (motion == null) {
+				return null;
+			}
+			return motion.AsReadOnly();
+		}
+	}
+	public Pose Pose {
+		get {
+			if (motion == null || motion.Count == 0) {
+				return null;
+			}
+			return motion[motion.Count-1];
+		}
+	}
 	public string Title { get; private set; }
 	public string Author { get; private set; }
 	public PresetType Type { get; private set; }
 
+	public Preset(List<Pose> motion, string title = "", string author = "") {
+		Type = PresetType.Animated;
+		this.motion = motion;
+		Title = title;
+		Author = author;
+	}
+	
 	public Preset(PresetType type = PresetType.Empty, Pose pose = null, string title = "", string author = "") {
 		Type = type;
-		Pose = pose;
+		if (pose != null) {
+			motion = new List<Pose>();
+			motion.Add(pose);
+		}
 		Title = title;
 		Author = author;
 	}
